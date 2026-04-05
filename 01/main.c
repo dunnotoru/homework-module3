72553ecf6f8d86b07de57021066e54016163fc63
dunnotoru <dunnotoru@yandex.ru>
2026-04-05 22:08:11 +0700

    fix 01/main.c

--- 01/main.c
+++ 01/main.c
@@ -1,31 +1,44 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-void parent_process_args(int argc, char **argv) {
-  pid_t pid = getpid();
-  for (int i = 1; i < argc; i+=2) {
-    char *endptr = NULL;
-    float value = strtof(argv[i], &endptr);
-    if (*endptr == '\0') {
-      printf("PARENT [%d]: \t\t", pid);
-      printf("Arg[%d] is a number: arg = %g, arg * 2 = %g\n", i, value, value * 2);
-    }
+void process_args(bool is_child, int argc, char **argv) {
+  int start = 1;
+  if (is_child) {
+    start = 2;
   }
-}
 
-void child_process_args(int argc, char **argv) {
   pid_t pid = getpid();
   pid_t ppid = getppid();
-  for (int i = 2; i < argc; i+=2) {
-    char *endptr = NULL;
-    strtof(argv[i], &endptr);
-    if (*endptr != '\0') {
+
+  for (int i = start; i < argc; i += 2) {
+    if (is_child) {
       printf("CHILD  [%d] of [%d]:  ", pid, ppid);
-      printf("Arg[%d] Is not number: %s\n",i, argv[i]);
+    } else {
+      printf("PARENT [%d]: \t\t", pid);
     }
+
+    printf("Arg[%d] is ", i);
+
+    char *endptr = NULL;
+    int value_int = strtol(argv[i], &endptr, 10);
+    if (*endptr == '\0') {
+      printf("an INTEGER number: arg = %d, arg * 2 = %d\n", value_int,
+             value_int * 2);
+      continue;
+    }
+
+    float value_float = strtof(argv[i], &endptr);
+    if (*endptr == '\0') {
+      printf("a FLOAT number: arg = %f, arg * 2 = %f\n", value_float,
+             value_float * 2);
+      continue;
+    }
+
+    printf("NOT a number: %s\n", argv[i]);
   }
 }
 
@@ -42,12 +55,12 @@ int main(int argc, char **argv) {
   case 0:
     // printf("CHILD: PID - %d\n", getpid());
     // printf("CHILD: PPID - %d\n", getppid());
-    child_process_args(argc, argv);
+    process_args(true, argc, argv);
     _exit(EXIT_SUCCESS);
   default:
     // printf("PARENT: PID - %d\n", getpid());
     // printf("PARENT: CHILD PID - %d\n", pid);
-    parent_process_args(argc, argv);
+    process_args(false, argc, argv);
     wait(&retval);
     printf("PARENT: CHILD EXITED WITH STATUS - %d\n", WEXITSTATUS(retval));
     exit(EXIT_SUCCESS);
---
+++ 02/main.c
@@ -0,0 +1,81 @@
+#include <bits/posix1_lim.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <limits.h>
+
+#include <string.h>
+
+#define BUFFER_SIZE 64
+#define MAX_COMMAND_SIZE 32000
+#define MAX_ARGV_SIZE 8192
+#define DEBUG
+#define SPACE_DELIM " \t\f\n\r\v"
+
+bool run(char cmd_buffer[MAX_COMMAND_SIZE]) {
+  // TODO: fix cringe
+  char *argv[MAX_ARGV_SIZE];
+  char *token = NULL;
+  token = strtok(cmd_buffer, SPACE_DELIM);
+  size_t i = 0;
+  argv[i++] = token;
+  do {
+    token = strtok(NULL, SPACE_DELIM);
+    argv[i++] = token;
+  } while (token != NULL && i < MAX_ARGV_SIZE);
+  
+  if (argv[0] == NULL) {
+    return false;
+  }
+
+  pid_t pid = fork();
+  switch (pid) {
+  case -1:
+    perror("Fork failed.");
+    return false;
+  case 0:
+    execvp(argv[0], argv);
+    break;
+  default:
+    return true;
+  }
+
+  return false;
+}
+
+int main() {
+
+  char hostname[HOST_NAME_MAX];
+  gethostname(hostname, sizeof(hostname));
+  const char* login = getlogin(); 
+  while (true) {
+    printf("[%s@%s] $ ", login, hostname);
+    char cmd_buffer[MAX_COMMAND_SIZE];
+
+    if (fgets(cmd_buffer, MAX_COMMAND_SIZE, stdin) == NULL) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (cmd_buffer[0] == '\n') {
+      continue;
+    }
+
+    cmd_buffer[strcspn(cmd_buffer, "\r\n")] = '\0';
+
+    if (strcmp(cmd_buffer, "exit") == 0) {
+      exit(EXIT_SUCCESS);
+    }
+
+    int retval;
+    if (run(cmd_buffer)) {
+      pid_t pid = wait(&retval);
+      // printf("CHILD PROCESS [%d] EXEC EXITET WITH STATUS - %d\n", pid,
+      //        WEXITSTATUS(retval));
+    } else {
+      printf("dsh: command not found: %s\n", cmd_buffer);
+    }
+  }
+}

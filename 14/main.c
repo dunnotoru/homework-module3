6e5516d3b4f8b07fc2103c927b7d998bce65cc84
dunnotoru <dunnotoru@yandex.ru>
2026-04-27 23:31:40 +0700

    14 raw both terminal and file outpu + args yayyyyy

--- 14/main.c
+++ 14/main.c
@@ -1,7 +1,12 @@
 #include <arpa/inet.h>
-#include <bits/types/siginfo_t.h>
+#include <bits/getopt_core.h>
 #include <ctype.h>
 #include <endian.h>
+#include <fcntl.h>
+#include <getopt.h>
+#include <iso646.h>
+#include <linux/limits.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -15,13 +20,19 @@
   "==========================================================================" \
   "================\n"
 
-int main(int argc, char **argv) {
-  int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
-  if (sockfd == -1) {
-    perror("Failed to open socket");
-    return EXIT_FAILURE;
-  }
+static struct option long_options[] = {
+    {"from", required_argument, NULL, 0},
+    {"to", required_argument, NULL, 0},
+    {NULL, 0, NULL, 0},
+};
 
+void usage_error(const char *name) {
+  fprintf(stderr, "Usage: %s --from <source ip> --to <destination ip>\n", name);
+  exit(EXIT_FAILURE);
+}
+
+int run(int fd, int sockfd, struct in_addr required_src_ip,
+        struct in_addr required_dest_ip) {
   while (1) {
     uint8_t buffer[1024] = {0};
     struct sockaddr_in from = {0};
@@ -33,10 +44,23 @@ int main(int argc, char **argv) {
       break;
     }
 
+    struct in_addr src_ip = {*(in_addr_t *)(buffer + 12)};
+    struct in_addr dest_ip = {*(in_addr_t *)(buffer + 16)};
+
+    if ((src_ip.s_addr != required_src_ip.s_addr && required_src_ip.s_addr != 0) ||
+        (dest_ip.s_addr != required_dest_ip.s_addr && required_dest_ip.s_addr != 0)) {
+      // printf("%b ? %b\n", required_src_ip.s_addr, src_ip.s_addr);
+      // printf("%b ? %b\n", required_dest_ip.s_addr, dest_ip.s_addr);
+      continue;
+    }
+    
+    if (write(fd, buffer, bytes_read) == -1) {
+      return -1;
+    }
+    
     buffer[bytes_read] = '\0';
     printf(P_SEPARATOR);
     printf("Bytes read %zu\n\n", bytes_read);
-    printf("Ip: %s\n", inet_ntoa(from.sin_addr));
     printf("########  ");
     for (ssize_t i = 0; i < 16; i++) {
       printf("%02zx ", i);
@@ -63,15 +87,9 @@ int main(int argc, char **argv) {
 
     printf("\n");
 
-    uint8_t ihl = *buffer;
-    uint32_t source = *(uint32_t *)(buffer + 12);
-    uint32_t dest = *(uint32_t *)(buffer + 16);
-    struct in_addr addr = {source};
-
     printf("IP header\n");
-    printf("Source IP: %s\n", inet_ntoa(addr));
-    addr.s_addr = dest;
-    printf("Dest IP:   %s\n", inet_ntoa(addr));
+    printf("Source IP: %s\n", inet_ntoa(src_ip));
+    printf("Dest IP:   %s\n", inet_ntoa(dest_ip));
 
     uint8_t udp_offset = 20;
     uint16_t source_port = *(uint16_t *)(buffer + udp_offset);
@@ -92,7 +110,67 @@ int main(int argc, char **argv) {
     printf(P_SEPARATOR);
   }
 
-  close(sockfd);
+  return 0;
+}
 
-  return EXIT_SUCCESS;
+int run_wrapper(struct in_addr required_src_ip,
+                struct in_addr required_dest_ip) {
+  int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
+  if (sockfd == -1) {
+    perror("Failed to open socket");
+  }
+
+  int fd = open(".dump", O_CREAT | O_WRONLY, 0644);
+  if (fd == -1) {
+    perror("Failed to open file");
+  }
+
+  int status = 0;
+  if (fd != -1 && sockfd != 1) {
+    status = run(fd, sockfd, required_src_ip, required_dest_ip);
+  }
+
+  if (fd != 1) {
+    close(fd);
+  }
+
+  if (sockfd != 1) {
+    close(sockfd);
+  }
+
+  return status;
+}
+
+int main(int argc, char **argv) {
+  struct in_addr required_src_ip = {0};
+  struct in_addr required_dest_ip = {0};
+
+  if (argc != 1) {
+    int opt_idx = 0;
+    int c = getopt_long(argc, argv, "", long_options, &opt_idx);
+    if (c == -1 || optarg == NULL) {
+      return EXIT_FAILURE;
+    }
+
+    if (inet_aton(optarg, &required_src_ip) == 0) {
+      usage_error(argv[0]);
+    }
+
+    c = getopt_long(argc, argv, "", long_options, &opt_idx);
+    if (c == -1 || optarg == NULL) {
+      return EXIT_FAILURE;
+    }
+
+    if (inet_aton(optarg, &required_dest_ip) == 0) {
+      usage_error(argv[0]);
+    }
+  }
+
+  printf("Listening...\n");
+  printf("from:     %s\n", inet_ntoa(required_src_ip));
+  printf("To:       %s\n", inet_ntoa(required_dest_ip));
+
+  int status = run_wrapper(required_src_ip, required_dest_ip);
+
+  return status == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
